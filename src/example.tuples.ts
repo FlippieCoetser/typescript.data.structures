83b133165fb262c815fcf100378755cc7dd173d5
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2024-05-02 14:24:50 +0200

    Added examples of how the different data structures should be filled

--- src/example.tuples.ts
+++ src/example.tuples.ts
@@ -7,3 +7,28 @@ let initTuples = () => {
   // Array of Tuples
   let pathway: pathway = [actor];
 };
+
+// Example of a complete actor with all attributes
+let maximum_tuple = [
+  "15b6679a-fd9d-4036-b1ab-af0b932fc903",
+  "Triage",
+  [0,0],
+  "🏥", 
+  "d5bc89b2-74ea-4d1a-a0ed-22f4de79a580",
+  "d4ae89b2-74ea-4d1a-a0ed-22f4de79a580", 
+  [
+    [
+      [
+        "log normal", 
+        [0.1640238, 0.4169375]
+      ]
+    ]
+  ]
+];
+// Example of a complete actor with only attributes required by D3 to plot it
+let minimum_tuple = [
+  "15b6679a-fd9d-4036-b1ab-af0b932fc903",
+  0,
+  0,
+  "🏥"
+];
