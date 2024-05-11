2c84d4dcb402d9d0869d240a8860eeb034356b78
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-11 11:10:03 -0700

    LINT: Auto Format and Indentation

--- wallaby.js
+++ wallaby.js
@@ -1,19 +1,15 @@
-module.exports = function(wallaby) {
-    return {
-        files: [
-            "src/**/*.ts"
-        ],
-        tests: [
-            "test/*.ts"
-        ],
-        trace: true,  
-        compilers: {
-            '**/*.ts': wallaby.compilers.typeScript({
-                "module": "es2020",
-                "target": "es2020",
-                "sourceMap": true,
-                "inlineSources": true
-            })
-        }
-    };
-}
+module.exports = function (wallaby) {
+  return {
+    files: ["src/**/*.ts"],
+    tests: ["test/*.ts"],
+    trace: true,
+    compilers: {
+      "**/*.ts": wallaby.compilers.typeScript({
+        module: "es2020",
+        target: "es2020",
+        sourceMap: true,
+        inlineSources: true,
+      }),
+    },
+  };
+};
