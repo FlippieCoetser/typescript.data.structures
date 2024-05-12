20411ad1e148c690e66d21dfef0363f2c92dd7e5
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-12 02:38:22 -0700

    TESTING: Update Test Naming Convention

--- test/Calculator.test.ts
+++ test/Calculator.test.ts
@@ -4,22 +4,22 @@ describe("Given Calculator imported", () => {
   it("then Calculator should exist", () => {
     expect(Calculator).toBeDefined();
   });
-  describe("When Calculator.Add(1, 2)", () => {
+  describe("when Calculator.Add(1, 2)", () => {
     it("then 3 should be returned", () => {
       expect(Calculator.Add(1, 2)).toBe(3);
     });
   });
-  describe("When Calculator.Subtract(2,1)", () => {
+  describe("when Calculator.Subtract(2,1)", () => {
     it("then 1 should be returned", () => {
       expect(Calculator.Subtract(2, 1)).toBe(1);
     });
   });
-  describe("When Calculator.Multiply(2,2)", () => {
+  describe("when Calculator.Multiply(2,2)", () => {
     it("then 4 should be returned", () => {
       expect(Calculator.Multiply(2, 2)).toBe(4);
     });
   });
-  describe("When Calculator.Divide(2,2)", () => {
+  describe("when Calculator.Divide(2,2)", () => {
     it("then 1 should be returned", () => {
       expect(Calculator.Divide(2, 2)).toBe(1);
     });
--- test/Structure.test.ts
+++ test/Structure.test.ts
@@ -2,16 +2,16 @@ import { Structure } from "../src/Structure.js";
 import { node } from "../src/Types.js";
 
 describe("Given Structure imported", () => {
-  it("Then Structure should exist", () => {
+  it("then Structure should exist", () => {
     expect(Structure).toBeDefined();
   });
   describe("Given Structure Exists", () => {
-    it("Then Structure should have create static member", () => {
+    it("then Structure should have create static member", () => {
       expect(Structure.create).toBeDefined();
     });
   });
   describe("When Structure.create is called with useTuple = true", () => {
-    it("Then it should return a tuple", () => {
+    it("then it should return a tuple", () => {
       let tuple = true;
       let input = Structure.create(tuple, "1", "John", 0, 0);
       let output: node = ["1", "John", 0, 0];
@@ -19,7 +19,7 @@ describe("Given Structure imported", () => {
     });
   });
   describe("When Structure.create is called with useTuple = false", () => {
-    it("Then it should return an object", () => {
+    it("then it should return an object", () => {
       let tuple = false;
       let input = Structure.create(tuple, "1", "John", 0, 0);
       let output: node = { id: "1", name: "John", x: 0, y: 0 };
