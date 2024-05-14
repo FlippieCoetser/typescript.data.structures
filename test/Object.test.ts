fbd9826250a53f9870c006a6c20bfc7fe443efe6
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-14 08:48:36 -0700

    TESTING: Swap to GraphNode Class

--- test/Object.test.ts
+++ test/Object.test.ts
@@ -1,30 +1,25 @@
 import { Simulator } from "../src/Simulator.js";
 import { Benchmark } from "./Benchmark.js";
-import { Node } from "../src/Types.js";
+import { GraphNode } from "../src/GraphNode.js";
 
 describe("Given Data Structure of Object type is used", () => {
+  let node: GraphNode;
+  let simulator: Simulator;
+  beforeEach(() => {
+    node = new GraphNode("object");
+    simulator = new Simulator();
+  });
   describe("when adding 1000 new nodes", () => {
-    let simulator: Simulator;
-    let createNode: () => Node;
-    beforeEach(() => {
-      simulator = new Simulator();
-      createNode = () => ({
-        id: (Math.random() * 10).toString(),
-        name: `node`,
-        x: Math.random() * 10,
-        y: Math.random() * 10,
-      });
-    });
-    it("then nodes should contain 1000 nodes taking a specific time load into graph", () => {
+    it("then nodes should contain 1000 nodes taking a specific time to load into graph", () => {
       let count = 1000;
       let nodes = Benchmark.Performance(() =>
-        simulator.generateNodes(count, createNode)
+        simulator.generateNodes(count, node.create)
       );
       expect(nodes.length).toEqual(count);
     });
     it("then nodes should contain 1000 nodes using a specific amount of memory", () => {
       let nodes = Benchmark.Memory(() =>
-        simulator.generateNodes(1000, createNode)
+        simulator.generateNodes(1000, node.create)
       );
       expect(nodes.length).toEqual(1000);
     });
