fa32e1230b39752cb232abf3637bee7659ad22e5
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-14 08:42:42 -0700

    GRAPHNODE: node.create() ReturnsNewNode -> PASS

--- src/GraphNode.ts
+++ src/GraphNode.ts
@@ -3,5 +3,13 @@ export class GraphNode {
   constructor(type: string) {
     this.type = type;
   }
-  public create = () => {};
+  public create = () =>
+    this.type === "object"
+      ? {
+          id: (Math.random() * 10).toString(),
+          name: `node`,
+          x: Math.random() * 10,
+          y: Math.random() * 10,
+        }
+      : null;
 }
--- src/Types.ts
+++ src/Types.ts
@@ -2,5 +2,5 @@ export type id = string;
 export type name = string;
 export type x = number;
 export type y = number;
-export type Node = [id, name, x, y] | { id: id; name: name; x: x; y: y };
+export type Node = [id, name, x, y] | { id: id; name: name; x: x; y: y } | null;
 export type Graph = Node[];
