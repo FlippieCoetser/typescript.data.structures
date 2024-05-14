1697619e7d6db534fb1feba9e09c868d1c2526f1
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-14 08:28:47 -0700

    GRAPHNODE: node[type] private property has value  `object` -> FAIL

---
+++ test/GraphNode.test.ts
@@ -0,0 +1,22 @@
+import { GraphNode } from "../src/GraphNode.js";
+
+describe("Given GraphNode is imported", () => {
+  it("then GraphNode should be defined", () => {
+    expect(GraphNode).toBeDefined();
+  });
+  describe("Given new GraphNode called node is instantiated", () => {
+    let node: GraphNode;
+    beforeEach(() => {
+      node = new GraphNode("object");
+    });
+    it("then node exist", () => {
+      expect(node).toBeDefined();
+    });
+    it("then node should contains private property type", () => {
+      expect(node["type"]).toBeDefined();
+    });
+    it("then node should contains property type with value object", () => {
+      expect(node["type"]).toEqual("object");
+    });
+  });
+});
