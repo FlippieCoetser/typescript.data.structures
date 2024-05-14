ca9a279496d80d6f76848ad8fea2aa28836a3175
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-14 08:13:37 -0700

    STRUCTURE: Update Naming Convention

--- src/Structure.ts
+++ src/Structure.ts
@@ -1,6 +1,6 @@
-import { node, id, name, x, y } from "./Types.js";
+import { Node, id, name, x, y } from "./Types.js";
 
 export class Structure {
-  static create = (tuple: boolean, id: id, name: name, x: x, y: y): node =>
+  static create = (tuple: boolean, id: id, name: name, x: x, y: y): Node =>
     tuple ? [id, name, x, y] : { id, name, x, y };
 }
