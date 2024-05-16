98111ba8a93fdac20c2915c9b4c895b56e3474ef
Flippie.Coetser <flippie.coetser@gmail.com>
2024-05-16 07:50:03 -0700

    DATA: Add Hybrid Example

--- data/example.object.ts
+++ data/example.object.ts
@@ -1,6 +1,6 @@
-import { node } from "../src/Types.js";
+import { Node } from "../src/Types.js";
 
-let node: node = {
+let node: Node = {
   id: "35c6779a-fd9d-4036-b1ab-af0b932fc903",
   name: "Start",
   x: 0,
