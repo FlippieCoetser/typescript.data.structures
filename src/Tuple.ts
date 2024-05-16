d698a4df7d521766ee6fbd119878732912fce8c9
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2024-05-16 18:51:35 +0200

    TUPLE: Added initial methods (from README), TODO: Create tests

--- src/Tuple.ts
+++ src/Tuple.ts
@@ -1,4 +1,19 @@
 export const tuple = {
+
+  // Perhaps using an enum would be more appropriate here
+  _keyToIndex: {
+    id: 0,
+    name: 1,
+    incoming: 2,
+    outgoing: 3,
+    metadata: 4,
+    ui: 5,
+  },
+
+  _small_keys: ["id", "name", "x", "y"],
+  _large_keys: ["id", "name", "incoming", "outgoing", "metadata", "ui"],
+
+
   small: {
     add: () => [
       (Math.random() * 10).toString(),
@@ -6,6 +21,54 @@ export const tuple = {
       Math.random() * 10,
       Math.random() * 10,
     ],
+
+    findAll: (nodes) => nodes,
+    findById: (nodes, id) => nodes.find((node) => node[0] === id),
+    findWhere: (nodes, key, value) => {
+      const index = tuple._small_keys.indexOf(key);
+      if (index === -1) return undefined;
+      return nodes.find((node) => node[index] === value);
+    },
+
+    updateAll: (nodes) => 
+      nodes.map((node) => {
+        // As discussed, this will increment the x value by 5
+        return [...node, node[2] + 5, node[3]];
+      }
+    ),
+
+    updateById: (nodes, id) => 
+      nodes.map((node) => {
+        if (node[0] === id) {
+          // As discussed, this will increment the x value by 5
+          return [...node, node[2] + 5, node[3]];
+        }
+        return node;
+      }
+    ),
+
+    updateWhere: (nodes, key, value) => {
+      const index = tuple._small_keys.indexOf(key);
+      if (index === -1) return nodes;
+
+      return nodes.map((node) => {
+        if (node[index] === value) {
+          // As discussed, this will increment the x value by 5
+          return [...node, node[2] + 5, node[3]];
+        }
+        return node;
+      });
+    },
+    
+    deleteAll: (nodes) => [],
+    deleteById: (nodes, id) => nodes.filter((node) => node[0] !== id),
+    deleteWhere: (nodes, key, value) => {
+      const index = tuple._small_keys.indexOf(key);
+      if (index === -1) return nodes;
+      return nodes.filter((node) => node[index] !== value);
+    },
+    
+
   },
   large: {
     add: () => [
@@ -16,5 +79,51 @@ export const tuple = {
       [[[["log normal"], [0.1640238, 0.4169375]]]],
       [[Math.random() * 10, Math.random() * 10], "icon"],
     ],
+
+    findAll: (nodes) => nodes,
+    findById: (nodes, id) => nodes.find((node) => node[0] === id),
+    findWhere: (nodes, key, value) => {
+      const index = tuple._large_keys.indexOf(key);
+      if (index === -1) return undefined;
+      return nodes.find((node) => node[index] === value);
+    },
+
+    updateAll: (nodes) => 
+      nodes.map((node) => {
+        // As discussed, this will increment the x value by 5
+        return [...node, node[2] + 5, node[3]];
+      }
+    ),
+
+    updateById: (nodes, id) => 
+      nodes.map((node) => {
+        if (node[0] === id) {
+          // As discussed, this will increment the x value by 5
+          return [...node, node[2] + 5, node[3]];
+        }
+        return node;
+      }
+    ),
+
+    updateWhere: (nodes, key, value) => {
+      const index = tuple._large_keys.indexOf(key);
+      if (index === -1) return nodes;
+
+      return nodes.map((node) => {
+        if (node[index] === value) {
+          // As discussed, this will increment the x value by 5
+          return [...node, node[2] + 5, node[3]];
+        }
+        return node;
+      });
+    },
+
+    deleteAll: (nodes) => [],
+    deleteById: (nodes, id) => nodes.filter((node) => node[0] !== id),
+    deleteWhere: (nodes, key, value) => {
+      const index = tuple._large_keys.indexOf(key);
+      if (index === -1) return nodes;
+      return nodes.filter((node) => node[index] !== value);
+    },
   },
 };
