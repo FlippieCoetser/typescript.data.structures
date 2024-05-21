bc1760c78c192ed40d93bb8cd5ef7b470b5573e3
Dimitriy Kruglikov <dimitriy.kruglikov@gmail.com>
2024-05-21 08:48:03 +0200

    TYPES: Updated the Object and Tuple types to include the icon property

--- src/Types.ts
+++ src/Types.ts
@@ -2,5 +2,9 @@ export type id = string;
 export type name = string;
 export type x = number;
 export type y = number;
-export type Node = [id, name, x, y] | { id: id; name: name; x: x; y: y } | null;
+export type icon = string;
+
+export type TupleNode = [id, name, x, y, icon];
+export type ObjectNode = { id: id; name: name; x: x; y: y; icon: icon;};
+export type Node = TupleNode | ObjectNode | null;
 export type Graph = Node[];
